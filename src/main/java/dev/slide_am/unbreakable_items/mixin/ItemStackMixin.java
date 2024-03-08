587be55382e795fd9163b172d9058ff7253c35fd
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-03-08 02:08:29 +0100

    Change Fabric Loader version to 0.14.23

--- src/main/java/dev/slide_am/unbreakable_items/mixin/ItemStackMixin.java
+++ src/main/java/dev/slide_am/unbreakable_items/mixin/ItemStackMixin.java
@@ -1,6 +1,7 @@
 package dev.slide_am.unbreakable_items.mixin;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Consumer;
-
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
 
